fd553c023a383ffa317a1f26ce358d711e70e2f1
alifrebat <alifrebat@gmail.com>
2026-05-05 16:41:41 -0300

    Ajustado as class da lista

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //PEGANDO ELEMENTOS DO DOM
 const formVeiculos = document.querySelector('#form-veiculos')
+const divListarVeiculo = document.querySelector('#div-veiculo')
 const btnEnviar = document.querySelector('#btn-enviar')
 const btnLimpar = document.querySelector('#btn-limpar')
 
@@ -7,7 +8,7 @@ const btnLimpar = document.querySelector('#btn-limpar')
 veiculos = []
 
 //CAPTURANDO O EVENTO DO SUBMIT
-formVeiculos.addEventListener('submit',  (evt) => {
+formVeiculos.addEventListener('submit', (evt) => {
     evt.preventDefault()
 
     const objFrmVeiculo = new FormData(formVeiculos)
@@ -25,15 +26,59 @@ formVeiculos.addEventListener('submit',  (evt) => {
     }
 
     cadastroVeiculo(veiculo)
-    
+
     console.log(veiculos.length)
-    
+
     formVeiculos.reset()
 
 })
 
+//ADICIONANDO ELEMENTO NO ARRAY
 const cadastroVeiculo = (objVeiculo) => {
-    if(objVeiculo !== null){
+    if (objVeiculo !== null) {
         veiculos.push(objVeiculo)
     }
+
+    listarVeiculos()
+}
+
+//LISTANDO OS ELEMENTOS DO ARRAY
+const listarVeiculos = () => {
+    divListarVeiculo.innerHTML = ''
+
+    veiculos.forEach((elem, i) => {
+        const divVeiculo = document.createElement('div')
+        divVeiculo.setAttribute('class', 'veiculo')
+        divVeiculo.innerHTML = `<span class="txt"> ${elem.modelo} </span> <span class="txt"> ${elem.marca} </span> <span class="txt alg"> ${elem.placa} </span> <span class="txt alg"> ${elem.ano}anos </span> <span class="vlr"> R$ ${elem.valor} </span> <span class="vlr"> R$ ${elem.valor} </span> <span class="vlr"> R$ ${parseFloat(elem.valor) + parseFloat(elem.valor)} </span>`
+
+        
+        const imgAlterar = document.createElement('img')
+        imgAlterar.setAttribute('src', 'imagens/btn_alterar.png')
+        imgAlterar.setAttribute('alt', 'Alterar')
+        imgAlterar.setAttribute('title', 'Alterar')
+        
+        imgAlterar.addEventListener('click', () => {
+            alert(`Em construção ${elem.idVeiculo}`)
+        })
+        
+        const spanImgAlterar = document.createElement('span')
+        spanImgAlterar.appendChild(imgAlterar)
+
+        const imgExcluir = document.createElement('img')
+        imgExcluir.setAttribute('src', 'imagens/btn_excluir.png')
+        imgExcluir.setAttribute('alt', 'Excluir')
+        imgExcluir.setAttribute('title', 'Excluir')
+
+        imgExcluir.addEventListener('click', () => {
+            alert(`Em construção ${elem.idVeiculo}`)
+        })
+
+        const spanImgExcluir = document.createElement('span')
+        spanImgExcluir.appendChild(imgExcluir)
+
+        divVeiculo.appendChild(spanImgAlterar)
+        divVeiculo.appendChild(spanImgExcluir)
+
+        divListarVeiculo.appendChild(divVeiculo)
+    });
 }
