391674b8603ec2cbd059bda1a5f08ea5b9607b76
alifrebat <alifrebat@gmail.com>
2026-05-05 15:50:36 -0300

    Capturado evento de submit do formulário

--- js/script.js
+++ js/script.js
@@ -5,3 +5,35 @@ const btnLimpar = document.querySelector('#btn-limpar')
 
 //ARRAY VEICULOS
 veiculos = []
+
+//CAPTURANDO O EVENTO DO SUBMIT
+formVeiculos.addEventListener('submit',  (evt) => {
+    evt.preventDefault()
+
+    const objFrmVeiculo = new FormData(formVeiculos)
+
+    const idsession = sessionStorage.getItem('idsessionveiculo') === null ? 0 : sessionStorage.getItem('idsessionveiculo')
+
+    veiculo = {
+        idVeiculo: idsession,
+        modelo: objFrmVeiculo.get('modelo'),
+        marca: objFrmVeiculo.get('marca'),
+        placa: objFrmVeiculo.get('placa'),
+        ano: objFrmVeiculo.get('ano'),
+        valor: objFrmVeiculo.get('valor'),
+        tipoCombustivel: objFrmVeiculo.get('tpCombustivel')
+    }
+
+    cadastroVeiculo(veiculo)
+    
+    console.log(veiculos.length)
+    
+    formVeiculos.reset()
+
+})
+
+const cadastroVeiculo = (objVeiculo) => {
+    if(objVeiculo !== null){
+        veiculos.push(objVeiculo)
+    }
+}
