53c090708f0faefb4cabf9e84f2da873afa3bf54
alifrebat <alifrebat@gmail.com>
2026-05-05 15:31:23 -0300

    Criado o arquivo script.js. Pego elementos do DOM e criado o array veiculos

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//PEGANDO ELEMENTOS DO DOM
+const formVeiculos = document.querySelector('#form-veiculos')
+const btnEnviar = document.querySelector('#btn-enviar')
+const btnLimpar = document.querySelector('#btn-limpar')
+
+//ARRAY VEICULOS
+veiculos = []
